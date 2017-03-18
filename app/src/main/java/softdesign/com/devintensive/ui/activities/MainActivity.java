8b9049f0f762e925e350846bb606ccc6403ff11d
Alexander <ag@799000.ru>
2017-03-18 10:15:03 +1000

    Initial commit

---
+++ app/src/main/java/softdesign/com/devintensive/ui/activities/MainActivity.java
@@ -0,0 +1,15 @@
+package softdesign.com.devintensive.ui.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import softdesign.com.devintensive.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
